98247db5ddb9709641f89ab036e5ad8e3ca15e55
Alexey Romanenkov <keksik888keksik@gmail.com>
2025-05-31 13:58:04 +0300

    frontend in progress

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="homePage"),
     path('registration', views.registration, name="registrationPage"),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
