319bc36724d6871274d2be812f6488abc84b33cf
Alexey Romanenkov <keksik888keksik@gmail.com>
2025-06-02 10:03:36 +0300

    registration full update + DB rework

--- main/forms.py
+++ main/forms.py
@@ -4,23 +4,32 @@ from django.forms import ModelForm,TextInput
 class RegistrationForm(ModelForm):
     class Meta:
         model=Contestant
-        fields=["email", "fullName","password"]
-        widgets={"email":TextInput(attrs={
-            'type':"email",
-            'class':"form-control",
-            'id':"floatingEmail",
-            'placeholder':"name@example.com"
-        }),
+        fields=["fullName", "organization","phone", "email"]
+        widgets={
         "fullName":TextInput(attrs={
             'type':"text",
             'class':"form-control",
-            'id':"floatingFullName",
-            'placeholder':"Your full name"
+            'id':'floatingFullName',
+            'placeholder':' '
         }),
-        "password":TextInput(attrs={
-            'type':"password",
+        "organization":TextInput(attrs={
+            'type':"text",
             'class':"form-control",
-            'id':"floatingPassword",
-            'placeholder':"Password"
+            'id':"floatingOrganization",
+            'placeholder':" "
         }),
+        "phone":TextInput(attrs={
+            'type':"tel",
+            'class':"form-control",
+            'id':"floatingPhone",
+            'data-phone-pattern': '+7 (###) ###-##-##',
+            'placeholder':" "
+        }),
+        "email":TextInput(attrs={
+            'type':"email",
+            'class':"form-control",
+            'id':"floatingEmail",
+            'placeholder':" "
+        })
+
         }
--- main/models.py
+++ main/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class Contestant(models.Model):
     email=models.EmailField(max_length=80, primary_key=True)
     fullName=models.CharField(max_length=150)
-    password=models.CharField(max_length=128)
+    phone=models.CharField(
+        max_length=20,
+        validators=[RegexValidator(regex=r'^(\+7|8)?[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$')]
+    )
+    organization=models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.email
